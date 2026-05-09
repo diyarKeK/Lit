060b2bf4f31716e654603b54c0209ffc2fd785e4
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-09 23:31:36 +0500

    Updated - analyzer.rs: bugs fixed and now by help of new enum TypeSource, semantic checking of ariphmethic works correctly and finds error in source files

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,8 +1,24 @@
 use std::collections::HashMap;
 use std::process;
 
+use crate::analyzer::TypeSource as TS;
 use crate::ast::*;
 use crate::generate_error;
+
+#[derive(Debug, Clone)]
+enum TypeSource {
+    Lit(Type),
+    Var(Type),
+}
+
+impl TypeSource {
+    fn get_type(&self) -> &Type {
+        match self {
+            TS::Lit(t) => t,
+            TS::Var(t) => t,
+        }
+    }
+}
 
 pub fn analyze(program: &Program) {
     for func in &program.funcs {
@@ -29,18 +45,24 @@ pub fn analyze(program: &Program) {
     }
 }
 
-fn infer_type(arena: &ExprArena, expr_id: ExprId, declared: &HashMap<String, Type>) -> Type {
-    match arena.get(expr_id) {
-        Expr::Unt(_) => Type::Unt,
-        Expr::Int(_) => Type::Int,
-        Expr::Float(_) => Type::Float,
-        Expr::Bool(_) => Type::Bool,
-        Expr::Str(_) => Type::Str,
+fn infer_type(
+    arena: &ExprArena,
+    id: ExprId,
+    declared: &HashMap<String, Type>
+) -> TypeSource {
+    match arena.get(id) {
+        Expr::Unt(_) => TS::Lit(Type::Unt),
+        Expr::Int(_) => TS::Lit(Type::Int),
+        Expr::Float(_) => TS::Lit(Type::Float),
+        Expr::Bool(_) => TS::Lit(Type::Bool),
+        Expr::Str(_) => TS::Lit(Type::Str),
 
         Expr::Var(name) => {
-            declared.get(name).unwrap_or_else(|| {
+            let _type = declared.get(name).unwrap_or_else(|| {
                 generate_error!("Variable '{}' is not declared", name);
-            }).clone()
+            }).clone();
+            
+            TS::Var(_type)
         }
 
         Expr::Binary { left, op, right} => {
@@ -50,17 +72,55 @@ fn infer_type(arena: &ExprArena, expr_id: ExprId, declared: &HashMap<String, Typ
             let resolved = resolve_binary_type(&left, &right);
 
             resolved.unwrap_or_else(|| {
-                generate_error!("Cannot apply operator '{}' for '{}' and '{}'", op, left, right);
+                generate_error!("Cannot apply operator '{}' for '{}' and '{}'", op, left.get_type(), right.get_type());
             })
         }
     }
 }
 
-fn resolve_binary_type(left_type: &Type, right_type: &Type) -> Option<Type> {
-    match (left_type, right_type) {
+fn resolve_binary_type(left: &TypeSource, right: &TypeSource) -> Option<TypeSource> {
+    match (left, right) {
+        (TS::Var(a), TS::Var(b)) => {
+            if a == b {
+                Some(TS::Var(a.clone()))
+            } else {
+                None
+            }
+        }
+
+        (TS::Var(a), TS::Lit(b)) |
+        (TS::Lit(b), TS::Var(a))
+            => {
+            if does_literal_num_fits_in(b, a) {
+                Some(TS::Var(a.clone()))
+            } else {
+                None
+            }
+        }
+
+        (TS::Lit(a), TS::Lit(b)) => {
+            numeric_tower(a, b).map(TS::Lit)
+        }
+    }
+}
+
+fn does_literal_num_fits_in(lit: &Type, var: &Type) -> bool {
+    matches!(
+        (var, lit),
+        (Type::Unt, Type::Unt) |
+        (Type::Int, Type::Unt) |
+        (Type::Int, Type::Int) |
+        (Type::Float, Type::Unt) |
+        (Type::Float, Type::Int) |
+        (Type::Float, Type::Float)
+    )
+}
+
+fn numeric_tower(a: &Type, b: &Type) -> Option<Type> {
+    match (a, b) {
         (a, b) if a == b => Some(a.clone()),
 
-        (Type::Unt, Type::Int) | (Type::Int, Type::Unt)
+        (Type::Int, Type::Unt) | (Type::Unt, Type::Int)
             => Some(Type::Int),
 
         (Type::Unt, Type::Float) | (Type::Float, Type::Unt) |
@@ -71,13 +131,18 @@ fn resolve_binary_type(left_type: &Type, right_type: &Type) -> Option<Type> {
     }
 }
 
-fn check_compat(var_type: &Type, expr_type: &Type, var_name: &String) {
-    let ok = match (var_type, expr_type) {
-        (a, b) if a == b => true,
+fn check_compat(
+    var_type: &Type,
+    expr_ts: &TypeSource,
+    var_name: &String
+) {
+    let ok = match (var_type, expr_ts) {
+        (a, TS::Var(b)) if a == b => true,
+        (a, TS::Lit(b)) if a == b => true,
 
-        (Type::Int, Type::Unt) => true,
-        (Type::Float, Type::Unt) => true,
-        (Type::Float, Type::Int) => true,
+        (Type::Int, TS::Lit(Type::Unt)) => true,
+        (Type::Float, TS::Lit(Type::Unt)) => true,
+        (Type::Float, TS::Lit(Type::Int)) => true,
 
         _ => false,
     };
@@ -85,12 +150,16 @@ fn check_compat(var_type: &Type, expr_type: &Type, var_name: &String) {
     if !ok {
         generate_error!(
             "Cannot assign '{}' to variable '{}' of type '{}'",
-            expr_type, var_name, var_type
+            expr_ts.get_type(), var_name, var_type
         )
     }
 }
 
-fn check_vars_declared(arena: &ExprArena, expr_id: ExprId, declared: &HashMap<String, Type>) {
+fn check_vars_declared(
+    arena: &ExprArena,
+    expr_id: ExprId,
+    declared: &HashMap<String, Type>
+) {
     match arena.get(expr_id) {
         Expr::Var(name) => {
             if !declared.contains_key(name) {
