aac876bf0e56e241163970ec301fe8ed2c3ff0f4
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-26 20:33:58 +0500

    Variant Plus renamed to Add, Minus to Sub and Rem to Mod; Added new variants: (Comparison Operators): EqEq - ==, NotEq - !=, (Arranging Operators) Gt - >, Lt - <, GtEq - >=, LtEq - <=, (Logical Operators): And - &, Or - |, Xor - ^; Added methods: is_arithmetic(), is_comparison(), is_arranging(), is_logical()

--- src/litc/ast/bin_op.rs
+++ src/litc/ast/bin_op.rs
@@ -2,21 +2,89 @@ use std::fmt;
 
 #[derive(Debug, Clone)]
 pub enum BinaryOp {
-    Plus,
-    Minus,
-    Mul,
-    Div,
-    Rem,
+    
+    // arithmetic
+    Add,    // `+`
+    Sub,    // `-`
+    Mul,    // `*`
+    Div,    // `/`
+    Mod,    // `%`
+    
+    // comparison
+    EqEq,   // `==`
+    NotEq,  // `!=`
+    Gt,     // `>`
+    Lt,     // `<`
+    GtEq,   // `>=`
+    LtEq,   // `<=`
+    
+    // logical
+    And,    // `&`
+    Or,     // `|`
+    Xor,    // `^`
+}
+
+impl BinaryOp {
+    pub fn is_arithmetic(&self) -> bool {
+        match self { 
+            BinaryOp::Add |
+            BinaryOp::Sub |
+            BinaryOp::Mul | 
+            BinaryOp::Div |
+            BinaryOp::Mod
+            => true,
+            _ => false,
+        }
+    }
+    
+    pub fn is_comparison(&self) -> bool {
+        match self {
+            BinaryOp::EqEq |
+            BinaryOp::NotEq
+            => true,
+            _ => false,
+        }
+    }
+    
+    pub fn is_arranging(&self) -> bool {
+        match self {
+            BinaryOp::Gt |
+            BinaryOp::Lt |
+            BinaryOp::GtEq |
+            BinaryOp::LtEq
+            => true,
+            _ => false,
+        }
+    }
+    
+    pub fn is_logical(&self) -> bool {
+        match self {
+            BinaryOp::And |
+            BinaryOp::Or |
+            BinaryOp::Xor
+            => true,
+            _ => false,
+        }
+    }
 }
 
 impl fmt::Display for BinaryOp {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            BinaryOp::Plus => write!(f, "+"),
-            BinaryOp::Minus => write!(f, "-"),
+            BinaryOp::Add => write!(f, "+"),
+            BinaryOp::Sub => write!(f, "-"),
             BinaryOp::Mul => write!(f, "*"),
             BinaryOp::Div => write!(f, "/"),
-            BinaryOp::Rem => write!(f, "%"),
+            BinaryOp::Mod => write!(f, "%"),
+            BinaryOp::EqEq => write!(f, "=="),
+            BinaryOp::NotEq => write!(f, "!="),
+            BinaryOp::Gt => write!(f, ">"),
+            BinaryOp::Lt => write!(f, "<"),
+            BinaryOp::GtEq => write!(f, ">="),
+            BinaryOp::LtEq => write!(f, "<="),
+            BinaryOp::And => write!(f, "&"),
+            BinaryOp::Or => write!(f, "|"),
+            BinaryOp::Xor => write!(f, "^"),
         }
     }
 }
