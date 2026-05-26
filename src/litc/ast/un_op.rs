32b979131dc6d46c7d9b76f71cec9a1221533b4f
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-26 20:27:10 +0500

    Added new variant: Not for numbers and booleans

--- src/litc/ast/un_op.rs
+++ src/litc/ast/un_op.rs
@@ -3,13 +3,15 @@ use std::fmt;
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
 pub enum UnaryOp {
-    Minus,
+    Minus,  // `-`
+    Not,    // `!`
 }
 
 impl fmt::Display for UnaryOp {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             UnaryOp::Minus => write!(f, "-"),
+            UnaryOp::Not => write!(f, "!"),
         }
     }
 }
