9df6a7097de6ef8eb615e10c35069827fd1c1187
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-27 18:04:47 +0500

    Added information about which variant is responsible for what

--- src/litc/ast/stmt.rs
+++ src/litc/ast/stmt.rs
@@ -3,6 +3,6 @@ use super::var_decl::VarDecl;
 
 #[derive(Debug)]
 pub enum Stmt {
-    Println(ExprId),
-    VarDecl(VarDecl),
+    Println(ExprId),     // `println()`
+    VarDecl(VarDecl),    // `<type> <name> = <expression>`
 }
