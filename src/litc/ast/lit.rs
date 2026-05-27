8d1f1bdbf52a6946dba2ebeb98ce05f595fdbd68
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-27 18:04:16 +0500

    Added information about which variant is responsible for what

--- src/litc/ast/lit.rs
+++ src/litc/ast/lit.rs
@@ -1,8 +1,8 @@
 #[derive(Debug, Clone)]
 pub enum Lit {
-    Unt(u64),
-    Int(i64),
-    Float(f64),
-    Bool(bool),
-    Str(String),
+    Unt(u64),      // literal unsigned 64-bit integer
+    Int(i64),      // literal signed 64-bit integer
+    Float(f64),    // literal 64-bit number with floating point
+    Bool(bool),    // literal boolean: `true` or `false`
+    Str(String),   // literal string
 }
