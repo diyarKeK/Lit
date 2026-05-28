c3b9a9f95104ac871f75f286b8d18d4628048e12
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-28 19:59:17 +0500

    Complete reconstruction of analyzer.rs: Added struct Analyzer which will contain link to expression arena and a field delcared_vars - is responsible for storaging declared variables. No more super complex functions like numeric tower here.

--- src/litc/analyzer/analyzer.rs
+++ src/litc/analyzer/analyzer.rs
@@ -4,180 +4,101 @@ use crate::ast::*;
 use crate::generate_error;
 
 pub fn analyze(program: &Program) {
+    let mut analyzer = Analyzer::new(&program.expr_arena);
+
     for func in &program.funcs {
-        let mut declared: HashMap<String, Type> = HashMap::new();
+        analyzer.analyze_func(func);
+    }
+}
+
+struct Analyzer<'a> {
+    arena: &'a ExprArena,
+    declared: HashMap<String, Type>,
+}
+
+impl<'a> Analyzer<'a> {
+    fn new(expr_arena: &'a ExprArena) -> Analyzer<'a> {
+        Analyzer {
+            arena: expr_arena,
+            declared: HashMap::new(),
+        }
+    }
+    fn analyze_func(&mut self, func: &FuncDef) {
+        self.declared.clear();
 
         for stmt in &func.body {
             match stmt {
                 Stmt::VarDecl(v) => {
-                    if declared.contains_key(&v.name) {
+                    if self.declared.contains_key(&v.name) {
                         generate_error!("Variable `{}` is already declared", v.name);
                     }
 
+                    let expr_type = self.infer_type(v.expr_id);
 
-                    let expr_type = infer_type(&program.expr_arena, v.expr_id, &declared);
-                    check_compat(&v._type, &expr_type, &v.name, has_variables(&program.expr_arena, v.expr_id));
+                    if expr_type != v._type {
+                        generate_error!(
+                            "Cannot assign {} value to variable `{}` of type `{}`",
+                            expr_type, v.name, v._type
+                        );
+                    }
 
-                    declared.insert(v.name.clone(), v._type.clone());
+                    self.declared.insert(v.name.clone(), v._type.clone());
                 }
-
-                Stmt::Println(expr) => {
-                    infer_type(&program.expr_arena, *expr, &declared);
+                Stmt::Println(expr_id) => {
+                    self.infer_type(*expr_id);
                 }
             }
         }
     }
-}
 
-fn has_variables(arena: &ExprArena, id: ExprId) -> bool {
-    let expr_node = arena.get(id);
-    let expr = &expr_node.expr;
+    fn infer_type(&self, id: ExprId) -> Type {
+        let expr_node = self.arena.get(id);
+        let expr = &expr_node.expr;
 
-    match expr {
-        Expr::Lit(_) => false,
-        Expr::Var(_) => true,
-        Expr::Binary { left, right, .. } => has_variables(arena, *left) || has_variables(arena, *right),
-        Expr::Unary { expr, .. } => has_variables(arena, *expr),
-    }
-}
+        use Lit::*;
+        match expr {
+            Expr::Lit(Unt(_)) => Type::Unt,
+            Expr::Lit(Int(_)) => Type::Int,
+            Expr::Lit(Float(_)) => Type::Float,
+            Expr::Lit(Bool(_)) => Type::Bool,
+            Expr::Lit(Str(_)) => Type::Str,
 
-fn infer_type(
-    arena: &ExprArena,
-    id: ExprId,
-    declared: &HashMap<String, Type>,
-) -> Type {
-    let expr_node = arena.get(id);
-    let expr = &expr_node.expr;
-    
-    match expr {
-        Expr::Lit(Lit::Unt(_)) => Type::Unt,
-        Expr::Lit(Lit::Int(_)) => Type::Int,
-        Expr::Lit(Lit::Float(_)) => Type::Float,
-        Expr::Lit(Lit::Bool(_)) => Type::Bool,
-        Expr::Lit(Lit::Str(_)) => Type::Str,
+            Expr::Var(name) => {
+                self.declared.get(name).unwrap_or_else(|| {
+                    generate_error!("Variable `{}` is not declared", name)
+                }).clone()
+            }
 
-        Expr::Var(name) => {
-            declared.get(name).unwrap_or_else(|| {
-                generate_error!("Variable `{}` is not declared", name);
-            }).clone()
+            Expr::Binary { op, left, right } => {
+                let left_ty = self.infer_type(*left);
+                let right_ty = self.infer_type(*right);
+
+                if left_ty != right_ty {
+                    generate_error!(
+                        "Cannot apply operator `{op}` for types: `{left}` and `{right}`",
+                        op = op, left = left_ty, right = right_ty
+                    )
+                }
+
+                if op.is_comparison() || op.is_arranging() {
+                    Type::Bool
+                } else {
+                    left_ty
+                }
+            }
+
+            Expr::Unary { op, expr } => {
+                let expr_ty = self.infer_type(*expr);
+
+                if expr_ty.is_num_type() || expr_ty == Type::Bool {
+                    expr_ty
+                } else {
+                    generate_error!(
+                        "Cannot apply unary operator `{op}` for type: `{type}`",
+                        op = op, type = expr_ty
+                    )
+                }
+            }
         }
-
-        Expr::Binary { op, left, right} => {
-            let left = infer_type(arena, *left, declared);
-            let right = infer_type(arena, *right, declared);
-
-            let resolved = resolve_binary_type(op, &left, &right);
-
-            resolved.unwrap_or_else(|| {
-                generate_error!("Cannot apply operator `{}` for types: `{}` and `{}`", op, left, right);
-            })
-        }
-
-        Expr::Unary { op, expr } => {
-            let expr_type = infer_type(arena, *expr, declared);
-            
-            let resolved = resolve_unary_type(&expr_type);
-
-            resolved.unwrap_or_else(|| {
-                generate_error!("Cannot apply unary operator `{}` for type: `{}`", op, expr_type);
-            })
-        }
-    }
-}
-
-fn resolve_binary_type(op: &BinaryOp, left: &Type, right: &Type) -> Option<Type> {
-    match op {
-        op if op.is_arithmetic() => match_arithmetic(left, right),
-        op if op.is_comparison() => match_comparison(left, right),
-        op if op.is_arranging() => match_arranging(left, right),
-        op if op.is_logical() => match_logical(left, right),
-
-        _ => unreachable!(),
-    }
-}
-
-fn match_arithmetic(left: &Type, right: &Type) -> Option<Type> {
-    let expr_type = numeric_tower(left, right);
-
-    if expr_type.is_some() {
-        Some(expr_type.unwrap())
-    } else {
-        None
-    }
-}
-
-fn match_comparison(left: &Type, right: &Type) -> Option<Type> {
-    if left == right {
-        Some(Type::Bool)
-    } else {
-        None
-    }
-}
-
-fn match_arranging(left: &Type, right: &Type) -> Option<Type> {
-    if left.is_num_type() && right.is_num_type() && left == right {
-        Some(Type::Bool)
-    } else {
-        None
-    }
-}
-
-fn match_logical(left: &Type, right: &Type) -> Option<Type> {
-    match (left, right) {
-        (Type::Bool, Type::Bool) => Some(Type::Bool),
-        _ => None
-    }
-}
-
-fn resolve_unary_type(expr: &Type) -> Option<Type> {
-    if expr.is_num_type() || *expr == Type::Bool {
-        Some(expr.clone())
-    } else {
-        None
-    }
-}
-
-fn numeric_tower(a: &Type, b: &Type) -> Option<Type> {
-    match (a, b) {
-        (Type::Unt, Type::Unt) => Some(Type::Unt),
-
-        (Type::Int, Type::Unt) | (Type::Unt, Type::Int) |
-        (Type::Int, Type::Int)
-            => Some(Type::Int),
-
-        (Type::Unt, Type::Float) | (Type::Float, Type::Unt) |
-        (Type::Int, Type::Float) | (Type::Float, Type::Int) |
-        (Type::Float, Type::Float)
-            => Some(Type::Float),
-
-        _ => None,
-    }
-}
-
-fn check_compat(
-    var_type: &Type,
-    expr_type: &Type,
-    var_name: &String,
-    contains_var: bool,
-) {
-    let ok = if contains_var {
-        var_type == expr_type
-    } else {
-        match (var_type, expr_type) {
-            (a, b) if a == b => true,
-
-            (Type::Int, Type::Unt) => true,
-            (Type::Float, Type::Unt) => true,
-            (Type::Float, Type::Int) => true,
-
-            _ => false,
-        }
-    };
-
-    if !ok {
-        generate_error!(
-            "Cannot assign {} value to variable `{}` of type `{}`",
-            expr_type, var_name, var_type
-        )
     }
 }
