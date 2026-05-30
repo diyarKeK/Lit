98dadb8e583274c4a4ba668a70885d6635c7efdd
_diyar_ <diyar.yusupov2233@gmail.com>
2026-05-30 18:15:45 +0500

    Added display in console.

--- src/litc/ast/lit.rs
+++ src/litc/ast/lit.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Clone)]
 pub enum Lit {
     Unt(u64),      // literal unsigned 64-bit integer
@@ -5,4 +7,16 @@ pub enum Lit {
     Float(f64),    // literal 64-bit number with floating point
     Bool(bool),    // literal boolean: `true` or `false`
     Str(String),   // literal string
+}
+
+impl fmt::Display for Lit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self { 
+            Lit::Unt(u) => write!(f, "Unt({})", u),
+            Lit::Int(i) => write!(f, "Int({})", i),
+            Lit::Float(fl) => write!(f, "Float({})", fl),
+            Lit::Bool(b) => write!(f, "Bool({})", b),
+            Lit::Str(s) => write!(f, "Str(\"{}\")", s),
+        }
+    }
 }
