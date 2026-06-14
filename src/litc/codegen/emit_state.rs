d342824b7c629d636d8196f3c1c375a578758446
_diyar_ <diyar.yusupov2233@gmail.com>
2026-06-15 00:36:45 +0500

    Removed field: fmt_idx and method: next_fmt_idx()

--- src/litc/codegen/emit_state.rs
+++ src/litc/codegen/emit_state.rs
@@ -1,13 +1,12 @@
 pub struct EmitState {
     reg: usize,
     str_idx: usize,
-    fmt_idx: usize,
 }
 
 impl EmitState {
     #[inline]
     pub fn new() -> EmitState {
-        EmitState { reg: 0, str_idx: 0, fmt_idx: 0 }
+        EmitState { reg: 0, str_idx: 0 }
     }
 
     pub fn next_reg(&mut self) -> usize {
@@ -19,12 +18,6 @@ impl EmitState {
     pub fn next_str_idx(&mut self) -> usize {
         let i = self.str_idx;
         self.str_idx += 1;
-        i
-    }
-
-    pub fn next_fmt_idx(&mut self) -> usize {
-        let i = self.fmt_idx;
-        self.fmt_idx += 1;
         i
     }
 }
