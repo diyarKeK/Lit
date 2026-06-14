ed9b40d792d26b0d747a43115191f81af92e0369
_diyar_ <diyar.yusupov2233@gmail.com>
2026-06-15 00:35:53 +0500

    Removed field: num_fmts. Because all numbers, that need to be formatted into string, do not need separate formatter values. Just %llu, %lld and %g enough for all numbers

--- src/litc/codegen/func_context.rs
+++ src/litc/codegen/func_context.rs
@@ -1,19 +1,15 @@
 use std::collections::HashMap;
 
-use super::LlvmType;
 use crate::ast::*;
-use crate::codegen::codegen::infer_llvm_type;
 
 pub struct FuncCtx {
     str_consts: Vec<String>,
-    num_fmts: Vec<String>,
     var_types: HashMap<String, Type>,
 }
 
 impl FuncCtx {
     pub fn build(func: &FuncDef, arena: &ExprArena) -> FuncCtx {
         let mut str_consts: Vec<String> = Vec::new();
-        let mut num_fmts = Vec::new();
         let mut var_types = HashMap::new();
 
         for stmt in &func.body {
@@ -33,32 +29,11 @@ impl FuncCtx {
             }
         }
 
-        for stmt in &func.body {
-            if let Stmt::Println(expr_id) = stmt {
-                let _type = infer_llvm_type(arena, *expr_id, &var_types);
-
-                let fmt = match _type {
-                    LlvmType::I64Unsigned => Some("%llu\n"),
-                    LlvmType::I64Signed => Some("%lld\n"),
-                    LlvmType::Double => Some("%g\n"),
-                    _ => None,
-                };
-
-                if let Some(f) = fmt {
-                    num_fmts.push(f.to_string());
-                }
-            }
-        }
-
-        FuncCtx { str_consts, num_fmts, var_types }
+        FuncCtx { str_consts, var_types }
     }
 
     pub fn get_string_consts(&self) -> &[String] {
         &self.str_consts
-    }
-
-    pub fn get_num_fmts(&self) -> &[String] {
-        &self.num_fmts
     }
 
     pub fn get_var_types(&self) -> &HashMap<String, Type> {
