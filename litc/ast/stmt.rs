59dfaee865288f6a976557996d0259f8aff7c712
_diyar_ <diyar.yusupov2233@gmail.com>
2026-06-17 22:40:00 +0500

    Nothing important

--- litc/ast/stmt.rs
+++ litc/ast/stmt.rs
@@ -3,6 +3,6 @@ use super::var_decl::VarDecl;
 
 #[derive(Debug)]
 pub enum Stmt {
-    Println(ExprId),     // `println()`
     VarDecl(VarDecl),    // `<type> <name> = <expression>`
+    Println(ExprId),     // `println()`
 }
