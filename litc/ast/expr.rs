1ebb751da38f9452c521873c572627e25e4c7a14
_diyar_ <diyar.yusupov2233@gmail.com>
2026-06-19 15:36:21 +0500

    Code refactoring: using tuples instead of structs

--- litc/ast/expr.rs
+++ litc/ast/expr.rs
@@ -9,8 +9,8 @@ pub enum Expr {
     Lit(Lit), // literal
     Var(String), // variable
     
-    Binary { op: BinaryOp, left: ExprId, right: ExprId }, // binary action, e.g. `2 + 2 * 2`
-    Unary { op: UnaryOp, expr: ExprId }, // unary action, e.g. `-a` where `a` is a variable
+    Binary(BinaryOp, ExprId, ExprId), // binary action, e.g. `2 + 2 * 2`
+    Unary(UnaryOp, ExprId), // unary action, e.g. `-a` where `a` is a variable
 
-    Cast { expr: ExprId, to: Type }, // cast action, e.g. `a as float` where variable `a` is of type `unt`
+    Cast(Type, ExprId), // cast action, e.g. `a as float` where variable `a` is of type `unt`
 }
